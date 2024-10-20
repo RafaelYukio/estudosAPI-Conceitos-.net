e133f20922425da80d7e051e7498f8afba21f08e
Rafael Yukio <rafaelyu@hotmail.com>
2024-10-20 17:51:23 -0300

    feat: EF with connection to SQL server

---
+++ 0. Simplest Project (DDD, Repository)/SimplestProject/src/SimplestProject.Infrastructure/Context/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplestProject.Domain.Entities;
+
+namespace SimplestProject.Infrastructure.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
